a5ef4ec200241bc6e6b32128439e5878187fdef2
GravesStudios <91075234+GravesStudios@users.noreply.github.com>
2023-04-02 00:27:29 -0500

    I have built out the UI for the Navbar

---
+++ site.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+
+
+function ShowMobileMenu() {
+    document.getElementById('mobileNav').style.display = "initial";
+};  
+
+
+
+function HideMobileMenu() {
+    document.getElementById('mobileNav').style.display = "none";
+}
